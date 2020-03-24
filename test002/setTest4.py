cff7e3f17a7fdb7ad88f31b625f5397924acb258
wangsong <wshappyday@sina.com>
2020-03-24 22:39:02 +0800

    2020.3.24 commit

---
+++ test002/setTest4.py
@@ -0,0 +1,9 @@
+set1 = {'aaa', 'bbb', 'ccc', 'ddd', 'eee'}
+set1.add("fff")
+print(set1)
+set1.update(["abc", "def"])
+print(set1)
+set1.remove("aaa")
+set1.add("zzz")
+print(set1)
+set1.discard("jjj")
